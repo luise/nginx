67503fa8d26440a7c356e5841aae2484ad2e05c3
Kevin Lin <kaikai526@gmail.com>
2017-03-01 08:40:58 -0600

    docker: Generate config file in the spec
    This patch removes our custom Dockerfile by generating the config file
    in the Javascript, rather than when the container boots.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var image = "quilt/nginx"
+var image = "nginx:1.10"
 
 exports.New = function(port) {
     port = port || 80;
@@ -8,11 +8,25 @@ exports.New = function(port) {
 
     // Create a Nginx Docker container, encapsulating it within the service "web_tier".
     var webTier = new Service("web_tier", [
-            new Container(image).withEnv({
-                PORT: port.toString(),
+            new Container(image).withFiles({
+                "/etc/nginx/conf.d/default.conf": buildConfig(port),
             })
     ]);
     publicInternet.connect(port, webTier);
 
     return webTier;
+}
+
+function buildConfig(port) {
+    var template = read("./default.tmpl");
+    return applyTemplate(template, {"port": port});
+}
+
+// applyTemplate replaces the keys defined by `vars` with their corresponding
+// values in `template`. A variable is denoted in the template using {{key}}.
+function applyTemplate(template, vars) {
+    for (k in vars) {
+        template = template.replace("{{"+k+"}}", vars[k]);
+    }
+    return template;
 }
