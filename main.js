8551ff4caf135eb7e9d7b136c02af2a9f6aef0bf
Kay Ousterhout <kay@quilt.io>
2017-06-29 15:49:37 -0700

    spec: Use (free) t2.micro instances
    Since the nginx spec is used in our examples, it's
    nice to use t2.micro instances, which are in Amazon's
    free tier (so many instance-hours can be used before
    the user is billed).

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const deployment = createDeployment({});
 // Setup the infrastructure.
 const baseMachine = new Machine({
   provider: 'Amazon',
+  size: 't2.micro',
+  // Be sure not to use spot instances, because t2.micro
+  // instances aren't available as spot instances.
+  preemptible: false,
   // Replace with your GitHub username to allow logging into machines.
   // sshKeys: githubKeys("CHANGE_ME"),
 });
