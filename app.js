05ef2f820c13212bdb9a5087e8aae3658fcfd3bf
Kay Ousterhout <kay@quilt.io>
2017-07-19 13:44:49 -0700

    blueprint: Lint
    This commit fixes all of the ESLint errors. There are no changes in
    functionality in this commit.

--- app.js
+++ app.js
@@ -2,37 +2,53 @@
 
 const fs = require('fs');
 const path = require('path');
-const {Service, Container, publicInternet} = require('@quilt/quilt');
+const {Container, Service, publicInternet} = require('@quilt/quilt');
 
-var image = "nginx:1.10"
+const image = 'nginx:1.10';
 
-exports.New = function(port) {
-    port = port || 80;
-    if (typeof port !== 'number') {
-        throw new Error("port must be a number");
-    }
+/**
+ * Creates and configures one container that runs nginx.
+ *
+ * @param {number} port The port that nginx should listen on. Defaults to
+ *     80 if not specified.
+ * @return {Service} A service that wraps the container running nginx.
+ */
+exports.createService = function(port) {
+  port = port || 80;
+  if (typeof port !== 'number') {
+    throw new Error('port must be a number');
+  }
 
-    // Create a Nginx Docker container, encapsulating it within the service "web_tier".
-    var webTier = new Service("web_tier", [
-            new Container(image).withFiles({
-                "/etc/nginx/conf.d/default.conf": buildConfig(port),
-            })
-    ]);
-    publicInternet.connect(port, webTier);
+  const nginxConfTemplate = fs.readFileSync(
+      path.join(__dirname, 'default.tmpl'), {encoding: 'utf8'});
+  const nginxConf = applyTemplate(nginxConfTemplate, {'port': port});
 
-    return webTier;
-}
+  // Create a Nginx Docker container, encapsulating it within the service
+  // "web_tier".
+  const webTier = new Service('web_tier', [
+    new Container(image).withFiles({
+      '/etc/nginx/conf.d/default.conf': nginxConf,
+    }),
+  ]);
+  publicInternet.connect(port, webTier);
 
-function buildConfig(port) {
-    var template = fs.readFileSync(path.join(__dirname, "default.tmpl"), {encoding: 'utf8'});
-    return applyTemplate(template, {"port": port});
-}
+  return webTier;
+};
 
-// applyTemplate replaces the keys defined by `vars` with their corresponding
-// values in `template`. A variable is denoted in the template using {{key}}.
+/**
+ * Replaces the keys defined by `vars` with their corresponding values in
+ * `template`. A variable is denoted in the template using {{key}}.
+ *
+ * @param {string} template A template containing variables to replace.
+ * @param {Object.<string, string>} vars Maps string variables to the value
+ *     that they should be replaced with.
+ * @return {string} `template` with all of the variables replaced.
+ */
 function applyTemplate(template, vars) {
-    for (var k in vars) {
-        template = template.replace("{{"+k+"}}", vars[k]);
+  for (let k in vars) {
+    if (typeof k === 'string') {
+      template = template.replace('{{'+k+'}}', vars[k]);
     }
-    return template;
+  }
+  return template;
 }
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const {createDeployment, Machine, githubKeys} = require('@quilt/quilt');
-var app = require('./app');
+const app = require('./app');
+const {Machine, createDeployment} = require('@quilt/quilt');
 
-var deployment = createDeployment({});
+const deployment = createDeployment({});
 
 // Setup the infrastructure.
-var baseMachine = new Machine({
-    provider: "Amazon",
-    // Replace with your GitHub username to allow logging into machines.
-    // sshKeys: githubKeys("CHANGE_ME"),
+const baseMachine = new Machine({
+  provider: 'Amazon',
+  // Replace with your GitHub username to allow logging into machines.
+  // sshKeys: githubKeys("CHANGE_ME"),
 });
 
 // Create Master and Worker Machines.
-deployment.deploy(baseMachine.asMaster())
+deployment.deploy(baseMachine.asMaster());
 deployment.deploy(baseMachine.asWorker().replicate(1));
 
-deployment.deploy(app.New());
+deployment.deploy(app.createService());
